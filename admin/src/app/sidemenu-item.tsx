1b429cc763780259d6cf98c5d353bf5ed7292d44
lulzshadowwalker <lulzshadowwalker@gmail.com>
2023-11-17 14:14:13 +0300

    sidemenu improvements

--- admin/src/app/sidemenu-item.tsx
+++ admin/src/app/sidemenu-item.tsx
@@ -16,17 +16,30 @@ export default function SideMenuItem({ href, icon }: SideMenuItemProps) {
   const isActive = path === href;
 
   return (
-    <Link
-      href={href ?? 'invalid-route'}
-      className={clsx({
-        'pointer-events-none': href === null,
-      })}
+    <div
+      className={`w-full ${clsx({
+        'cursor-not-allowed': href === null,
+      })}`}
     >
-      <FontAwesomeIcon
-        icon={icon}
-        size={'lg'}
-        className={`${isActive ? 'text-on-primary' : 'text-muted-700'}`}
-      />
-    </Link>
+      <Link
+        href={href ?? 'invalid-route'}
+        className={`group flex items-center justify-center h-[3.4rem] w-full ${clsx(
+          {
+            'pointer-events-none': href === null,
+          },
+        )}`}
+      >
+        {isActive && (
+          <div className='w-2 h-full bg-black rounded-se-md rounded-ee-md' />
+        )}
+        <FontAwesomeIcon
+          icon={icon}
+          size={'lg'}
+          className={`m-auto group-hover:text-on-primary transition-all ${
+            isActive ? 'text-on-primary' : 'text-muted-700'
+          }`}
+        />
+      </Link>
+    </div>
   );
 }
